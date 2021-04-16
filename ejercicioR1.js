2df29fddf3fdd0d9b96c5e0f6f30065e4405babf
marthajuliaOr <81401385+marthajuliaOr@users.noreply.github.com>
2021-04-15 21:52:41 -0700

    Update ejercicioR1.js

--- ejercicioR1.js
+++ ejercicioR1.js
@@ -18,55 +18,55 @@ var codigo = prompt ( "Porfavor, ingresa el codigo del producto: " ); //este val
 var pago = prompt ( "Porfavor, ingresa la cantidad a pagar: " ); //este valor será definido por el usuario en terminal
 
 //chocolate
-if ( ( codigo == "xc23" ) && ( pago == 12.5 ) ) { //si el código es "xc23" y el pago es exacto, 
+if (( codigo == "xc23" ) && ( pago == 12.5 )) { //si el código es "xc23" y el pago es exacto, 
     console.log ( "¡Compra de chocolate exitosa!" ); //se imprimirá el mensaje de "¡Compra de chocolate exitosa!"
 }
-else if ( ( codigo == "xc23" ) && ( pago > 12.5 ) ) { //si el código es "xc23" y el pago es mayor al obligatorio, 
+else if (( codigo == "xc23" ) && ( pago > 12.5 )) { //si el código es "xc23" y el pago es mayor al obligatorio, 
     console.log ( "¡Compra de chocolate exitosa!" ); //se imprimirá el mensaje de "¡Compra de chocolate exitosa!"
-    console.log ( "Su cambio es " + ( pago - 12.5 ) );//y el cambio que se debe entregar
+    console.log ( "Su cambio es " + ( pago - 12.5 ));//y el cambio que se debe entregar
 }
-else if ( ( codigo == "xc23" ) && ( pago < 12.5 ) ) { //si el código  es "xc23" y el pago es insuficiente,, 
+else if (( codigo == "xc23" ) && ( pago < 12.5 )) { //si el código  es "xc23" y el pago es insuficiente,, 
     console.log ( "¡Compra de chocolate denegada!" ); //se imprimirá el mensaje de error
     console.log ( "Porfavor, revise la informaión ingresada" ); //y dará otro intento al usuario de ingrsear algún valor correcto
 }
 
 //paleta (pasa lo mismo que arriba pero con un producto, codigo y pago distinto)
-else if ( ( codigo == "c4f2" ) && ( pago == 5.75 ) ) {
+else if (( codigo == "c4f2" ) && ( pago == 5.75 )) {
     console.log ( "¡Compra de paleta exitosa! ");
 }
-else if ( ( codigo == "c4f2") && (pago > 5.75) ) {
+else if (( codigo == "c4f2") && ( pago > 5.75 )) {
     console.log ( "¡Compra de paleta exitosa!" );
-    console.log ( "Su cambio es: " + ( pago - 5.75 ) );
+    console.log ( "Su cambio es: " + ( pago - 5.75 ));
 }
-else if ( ( codigo == "c4f2" ) && ( pago < 5.75 ) ) {
+else if (( codigo == "c4f2" ) && ( pago < 5.75 )) {
     console.log ( "¡Compra de paleta denegada!" );
     console.log ( "Porfavor, revise la informaión ingresada" );
 }
 
 //chicle (pasa lo mismo que arriba pero con un producto, codigo y pago distinto)
-else if ( ( codigo == "gt74" ) && ( pago == 4.25 ) ) {
+else if (( codigo == "gt74" ) && ( pago == 4.25 )) {
     console.log ( "¡Compra de chicle exitosa! " );
 }
-else if ( ( codigo == "gt74" ) && ( pago > 4.25 ) ) {
+else if (( codigo == "gt74" ) && ( pago > 4.25 )) {
     console.log ( "¡Compra de chicle exitosa!" );
-    console.log ( "Su cambio es " + (pago - 4.25) );
+    console.log ( "Su cambio es " + (pago - 4.25));
 }
-else if ( ( codigo == "gt74" ) && ( pago < 4.25 ) ) {
+else if (( codigo == "gt74" ) && ( pago < 4.25 )) {
     console.log ( "¡Compra de chicle denegada!" );
     console.log ( "Porfavor, revise la informaión ingresada" );
 }
 
 //galleta (pasa lo mismo que arriba pero con un producto, codigo y pago distinto)
-else if ( ( codigo == "l3k8" ) && ( pago == 9.10 ) ) {
+else if (( codigo == "l3k8" ) && ( pago == 9.10 )) {
     console.log ( "¡Compra de galleta exitosa! ");
 }
-else if ( ( codigo == "l3k8" ) && ( pago > 9.10 ) ) {
+else if (( codigo == "l3k8" ) && ( pago > 9.10 )) {
     console.log ( "¡Compra de galleta exitosa!" );
-    console.log ( "Su cambio es " + (pago - 9.10) );
+    console.log ( "Su cambio es " + (pago - 9.10));
 }
-else if ( ( codigo == "l3k8" ) && ( pago < 9.10 ) ) {
+else if (( codigo == "l3k8" ) && ( pago < 9.10 )) {
     console.log ( "¡Compra de galleta denegada!" );
-    console.log ( "Porfavor, revise la informaión ingresada" );
+    console.log ( "Porfavor, revise la información ingresada" );
 }
 
 /*esta condición la agregue por si el código ingresado no es correcto,
@@ -74,7 +74,7 @@ else if ( ( codigo == "l3k8" ) && ( pago < 9.10 ) ) {
   el codigo no debe ser existente dentro de lo válido */
 
 else {
-    console.log ("Porfavor, ingrese un código válido"); 
+    console.log ( "Porfavor, ingrese un código válido" ); 
 }
 
 /* Creo que esto podría resolverse también con un condicional switch, o incluso mostrando las opciones de compra y,
